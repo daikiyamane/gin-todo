96b8b0de50cec255525404e6fb5e2152b1de5d0d
山根大輝 <daiki04752@gmail.com>
2020-11-24 20:47:48 +0900

    :new todo.go

---
+++ entity/todo.go
@@ -0,0 +1,13 @@
+package entity
+
+import (
+	"gorm.io/gorm"
+)
+
+//Todo is Model
+type Todo struct {
+	gorm.Model
+	CreatedBy string `form:"name" gorm:"NOT NULL"`
+	Content   string `form:"content" gorm:"NOT NULL"`
+	Done      bool
+}
